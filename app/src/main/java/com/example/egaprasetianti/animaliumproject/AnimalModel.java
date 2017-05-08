10219014a2649823bc7e1a7d9778145391e59f83
Ega Prasetianti <eprasetianti@gmail.com>
2017-05-08 20:25:50 +0700

    belajar list

---
+++ app/src/main/java/com/example/egaprasetianti/animaliumproject/AnimalModel.java
@@ -0,0 +1,28 @@
+package com.example.egaprasetianti.animaliumproject;
+
+/**
+ * Created by Ega Prasetianti on 5/7/2017.
+ */
+
+public class AnimalModel {
+    String namaIndo, namaEng;
+    int gambar;
+
+    public String getNamaIndo() {
+        return namaIndo;
+    }
+
+    public String getNamaEng() {
+        return namaEng;
+    }
+
+    public int getGambar() {
+        return gambar;
+    }
+
+    public AnimalModel(String namaIndo, String namaEng, int gambar){
+        this.namaIndo = namaIndo;
+        this.namaEng = namaEng;
+        this.gambar = gambar;
+    }
+}
